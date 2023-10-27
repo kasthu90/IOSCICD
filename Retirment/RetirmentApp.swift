aec425f4665fc8b44c7ed6cbbd1a6d98703a1175
Kasthuri <tkasthu.90@gmail.com>
2023-10-27 11:02:17 +1100

    Initial Commit

---
+++ Retirment/RetirmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirmentApp.swift
+//  Retirment
+//
+//  Created by Kasthuri Tandavarayane on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RetirmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
